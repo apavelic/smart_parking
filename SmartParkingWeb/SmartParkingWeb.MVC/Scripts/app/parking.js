d3adfad36c8148292d5787a8790a3e07563f6fdf
Andrej Pavelić <pavelic.andrej@gmail.com>
2017-07-18 14:17:03 +0200

    style changed on parking and report view

--- SmartParkingWeb/SmartParkingWeb.MVC/Scripts/app/parking.js
+++ SmartParkingWeb/SmartParkingWeb.MVC/Scripts/app/parking.js
@@ -13,10 +13,16 @@
                     var id = value.Id;
                     var state = value.State;
 
+                    $("#lblState_" + id).removeClass("free").remove("taken");
+
                     if (state == 0) {
                         $("#" + id).css("background", "green");
+                        $("#lblState_" + id).css("color", "green");
+                        $("#lblState_" + id).html("FREE");
                     } else {
                         $("#" + id).css("background", "red");
+                        $("#lblState_" + id).css("color", "red");
+                        $("#lblState_" + id).html("TAKEN");
                     }
                 });
             }
